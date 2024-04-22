c610f38827c7a1b912e83b9d21b67a82866b9e62
AiINFOX <aiinfox@AiINFOXs-MacBook-Pro.local>
2024-04-22 23:24:43 +0530

    types

--- src/app/shoe/shoe.tsx
+++ src/app/shoe/shoe.tsx
@@ -26,18 +26,20 @@ export default function ShoeFun() {
 }
 
 function Shoe() {
-  const ref = useRef()
+  const ref:any = useRef()
   const snap = useSnapshot(state)
-  const { nodes, materials } = useGLTF("shoe-draco.glb")
+  const { nodes, materials }: any = useGLTF("shoe-draco.glb")
   const [hovered, set] = useState(null)
 
   useFrame((state) => {
-    const t = state.clock.getElapsedTime()
-    ref.current.rotation.set(Math.cos(t / 4) / 8, Math.sin(t / 4) / 8, -0.2 - (1 + Math.sin(t / 1.5)) / 20)
-    ref.current.position.y = (1 + Math.sin(t / 1.5)) / 10
+    const t = state.clock.getElapsedTime();
+    if(ref.current) {
+        ref.current.rotation.set(Math.cos(t / 4) / 8, Math.sin(t / 4) / 8, -0.2 - (1 + Math.sin(t / 1.5)) / 20)
+        ref.current.position.y = (1 + Math.sin(t / 1.5)) / 10
+    }
   })
 
-  useEffect(() => {
+  useEffect((): any => {
     const cursor = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0)"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><g filter="url(#filter0_d)"><path d="M29.5 47C39.165 47 47 39.165 47 29.5S39.165 12 29.5 12 12 19.835 12 29.5 19.835 47 29.5 47z" fill="${snap.items[hovered]}"/></g><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/><text fill="#000" style="#fff-space:pre" font-family="Inter var, sans-serif" font-size="10" letter-spacing="-.01em"><tspan x="35" y="63">${hovered}</tspan></text></g><defs><clipPath id="clip0"><path fill="#fff" d="M0 0h64v64H0z"/></clipPath><filter id="filter0_d" x="6" y="8" width="47" height="47" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/><feOffset dy="2"/><feGaussianBlur stdDeviation="3"/><feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0"/><feBlend in2="BackgroundImageFix" result="effect1_dropShadow"/><feBlend in="SourceGraphic" in2="effect1_dropShadow" result="shape"/></filter></defs></svg>`
     const auto = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/></svg>`
     if (hovered) {
@@ -49,10 +51,10 @@ function Shoe() {
   return (
     <group
       ref={ref}
-      onPointerOver={(e) => (e.stopPropagation(), set(e.object.material.name))}
+      onPointerOver={(e: any) => (e.stopPropagation(), set(e.object.material.name))}
       onPointerOut={(e) => e.intersections.length === 0 && set(null)}
       onPointerMissed={() => (state.current = null)}
-      onClick={(e) => (e.stopPropagation(), (state.current = e.object.material.name))}>
+      onClick={(e: any) => (e.stopPropagation(), (state.current = e.object.material.name))}>
       <mesh receiveShadow castShadow geometry={nodes.shoe.geometry} material={materials.laces} material-color={snap.items.laces} />
       <mesh receiveShadow castShadow geometry={nodes.shoe_1.geometry} material={materials.mesh} material-color={snap.items.mesh} />
       <mesh receiveShadow castShadow geometry={nodes.shoe_2.geometry} material={materials.caps} material-color={snap.items.caps} />
@@ -66,7 +68,7 @@ function Shoe() {
 }
 
 function Picker() {
-  const snap = useSnapshot(state)
+  const snap: any = useSnapshot(state)
   return (
     <div style={{ display: snap.current ? "block" : "none" }}>
       <HexColorPicker className="picker" color={snap.items[snap.current]} onChange={(color) => (state.items[snap.current] = color)} />
